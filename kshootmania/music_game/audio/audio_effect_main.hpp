3b25784cd9a96541bb4ea18a4d246f0d4ccfd1bc
masaka <masaka@mz-kb.com>
2022-06-04 13:21:36 +0900

    Remove unused AudioEffectUpdateInfo

--- kshootmania/music_game/audio/audio_effect_main.hpp
+++ kshootmania/music_game/audio/audio_effect_main.hpp
@@ -1,15 +1,12 @@
 ﻿#pragma once
-#include "audio_effect_update_info.hpp"
 #include "bgm.hpp"
+#include "kson/chart_data.hpp"
 #include "kson/util/timing_utils.hpp"
 
 namespace MusicGame::Audio
 {
 	class AudioEffectMain
 	{
-	private:
-		AudioEffectUpdateInfo m_audioEffectUpdateInfo;
-
 	public:
 		AudioEffectMain(const kson::ChartData& chartData);
 
