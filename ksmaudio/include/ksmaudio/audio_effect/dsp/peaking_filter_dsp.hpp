c2763143b9b26f62e0ac37f8795c51ad2ea76f29
masaka <masaka@mz-kb.com>
2022-12-23 00:11:09 +0900

    Add PeakingFilterValueController

--- ksmaudio/include/ksmaudio/audio_effect/dsp/peaking_filter_dsp.hpp
+++ ksmaudio/include/ksmaudio/audio_effect/dsp/peaking_filter_dsp.hpp
@@ -7,12 +7,40 @@
 
 namespace ksmaudio::AudioEffect
 {
+	namespace detail
+	{
+		class PeakingFilterValueController
+		{
+		private:
+			detail::LinearEasing<float> m_vEasing;
+			std::optional<float> m_prevVInGainUpdate = std::nullopt;
+			float m_baseGainDb = 0.0f;
+			float m_freq = 1000.0f;
+			bool m_updated = true;
+
+		public:
+			PeakingFilterValueController();
+
+			void updateGain(float v);
+
+			void updateFreq(float v);
+
+			bool popUpdated();
+
+			float baseGainDb() const;
+
+			float freq() const;
+
+			bool mixSkipped() const;
+		};
+	}
+
 	class PeakingFilterDSP
 	{
 	private:
 		const DSPCommonInfo m_info;
 		std::array<detail::BiquadFilter<float>, 2> m_peakingFilters;
-		detail::LinearEasing<float> m_vEasing;
+		detail::PeakingFilterValueController m_valueController;
 
 	public:
 		explicit PeakingFilterDSP(const DSPCommonInfo& info);
--- ksmaudio/src/audio_effect/dsp/peaking_filter_dsp.cpp
+++ ksmaudio/src/audio_effect/dsp/peaking_filter_dsp.cpp
@@ -29,7 +29,7 @@ namespace ksmaudio::AudioEffect
 		}
 
 		// TODO: freq、freq_maxの値を加味する
-		float GetPeakingFilterGainDb(float v, float gainRate)
+		float GetPeakingFilterBaseGainDb(float v)
 		{
 			// HSP版での計算式を再現したもの:
 			// https://github.com/m4saka/kshootmania-v1-hsp/blob/08275836547c7792a6d4f59037e56e947f2979c3/src/scene/play/play_audio_effects.hsp#L974
@@ -38,13 +38,65 @@ namespace ksmaudio::AudioEffect
 			const float midGain = 3.0f * (1.0f - std::abs(v - 0.35f) / 0.35f);
 			constexpr float kHiGainCutThreshold = 0.8f;
 			const float hiGainCut = (v > kHiGainCutThreshold) ? -3.5f * (v - kHiGainCutThreshold) / (1.0f - kHiGainCutThreshold) : 0.0f;
-			return std::clamp(baseGain + midGain + hiGainCut, 0.0f, 18.0f) * gainRate / 0.5f;
+			return std::clamp(baseGain + midGain + hiGainCut, 0.0f, 18.0f) / 0.5f;
+		}
+	}
+
+	namespace detail
+	{
+		PeakingFilterValueController::PeakingFilterValueController()
+			: m_vEasing(kVEasingSpeed)
+		{
+		}
+
+		void PeakingFilterValueController::updateGain(float v)
+		{
+			if (v != m_prevVInGainUpdate)
+			{
+				// gainの値はイージング適用前のものをもとに計算する
+				// (直角時に一瞬低域が強調されてノイズが入るのを回避するため)
+				m_baseGainDb = GetPeakingFilterBaseGainDb(v);
+				m_prevVInGainUpdate = v;
+				m_updated = true;
+			}
+		}
+
+		void PeakingFilterValueController::updateFreq(float v)
+		{
+			// 値が飛ぶことでノイズが入らないようvの値に対して線形のイージングを入れる
+			const bool valueUpdated = m_vEasing.update(v);
+			if (valueUpdated)
+			{
+				const float easedV = m_vEasing.value();
+				m_freq = GetPeakingFilterFreqValue(easedV);
+				m_updated = true;
+			}
+		}
+
+		bool PeakingFilterValueController::popUpdated()
+		{
+			return std::exchange(m_updated, false);
+		}
+
+		float PeakingFilterValueController::baseGainDb() const
+		{
+			return m_baseGainDb;
+		}
+
+		float PeakingFilterValueController::freq() const
+		{
+			return m_freq;
+		}
+
+		bool PeakingFilterValueController::mixSkipped() const
+		{
+			// 低周波数に対しては適用しない
+			return m_freq < kFreqThresholdMin;
 		}
 	}
 
 	PeakingFilterDSP::PeakingFilterDSP(const DSPCommonInfo& info)
 		: m_info(info)
-		, m_vEasing(kVEasingSpeed)
 	{
 	}
 
@@ -56,29 +108,23 @@ namespace ksmaudio::AudioEffect
 		}
 
 		const float fSampleRate = static_cast<float>(m_info.sampleRate);
-		const float gainDb = GetPeakingFilterGainDb(params.v, params.gainRate);
 		const bool isBypassed = bypass || params.mix == 0.0f; // 切り替え時のノイズ回避のためにbypass状態でも処理自体はする
 
 		assert(dataSize % m_info.numChannels == 0U);
 		const std::size_t frameSize = dataSize / m_info.numChannels;
-		float freq = GetPeakingFilterFreqValue(m_vEasing.value());
-		bool mixSkipped = isBypassed || freq < kFreqThresholdMin; // 低周波数に対しては適用しない
+		m_valueController.updateGain(params.v);
 		for (std::size_t i = 0U; i < frameSize; ++i)
 		{
-			// 値が飛ぶことでノイズが入らないようvの値に対して線形のイージングを入れる
-			const bool vUpdated = m_vEasing.update(params.v);
-			if (vUpdated)
-			{
-				freq = GetPeakingFilterFreqValue(m_vEasing.value());
-				mixSkipped = isBypassed || freq < kFreqThresholdMin; // 低周波数に対しては適用しない
-			}
+			m_valueController.updateFreq(params.v);
+			const bool mixSkipped = isBypassed || m_valueController.mixSkipped();
+			const bool valueUpdated = m_valueController.popUpdated();
 
 			// 各チャンネルにフィルタを適用
 			for (std::size_t ch = 0U; ch < m_info.numChannels; ++ch)
 			{
-				if (vUpdated)
+				if (valueUpdated)
 				{
-					m_peakingFilters[ch].setPeakingFilter(freq, params.bandwidth, gainDb, fSampleRate);
+					m_peakingFilters[ch].setPeakingFilter(m_valueController.freq(), params.bandwidth, m_valueController.baseGainDb() * params.gainRate, fSampleRate);
 				}
 
 				const float wet = m_peakingFilters[ch].process(*pData);
