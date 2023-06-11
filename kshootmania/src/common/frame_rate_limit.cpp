7dd884661239aa62e224eff42eb1381823cfc9c9
masaka <masaka@mz-kb.com>
2023-06-12 01:11:30 +0900

    Fix frame rate drops in particular environments

--- kshootmania/src/common/frame_rate_limit.cpp
+++ kshootmania/src/common/frame_rate_limit.cpp
@@ -1,21 +1,27 @@
 ﻿#include "frame_rate_limit.hpp"
 
+namespace
+{
+	constexpr std::chrono::steady_clock::duration kMaxDrift = 10ms;
+}
+
 FrameRateLimit::FrameRateLimit(int32 targetFPS)
 	: m_targetFPS(targetFPS)
-	, m_sleepUntil(std::chrono::steady_clock::now())
+	, m_sleepUntil(std::chrono::steady_clock::now() - kMaxDrift)
 {
 }
 
 void FrameRateLimit::postPresent()
 {
 	// 次フレームまでのsleepの終了時間を決める
-	m_sleepUntil += std::chrono::duration_cast<std::chrono::nanoseconds>(std::chrono::seconds(1)) / m_targetFPS;
+	m_sleepUntil += std::chrono::duration_cast<std::chrono::steady_clock::duration>(1s) / m_targetFPS;
 
-	// 目標フレームレートに届かなかったときにm_sleepUntilが現在時間より過去に離れていかないよう現在時間以降にする
-	const auto now = std::chrono::steady_clock::now();
-	if (m_sleepUntil < now)
+	// 目標フレームレートに届かなかったときにm_sleepUntilが現在時間より過去に離れていかないよう下限を設ける
+	// (環境によっては現在時刻ちょうどまでのsleepでも長めの時間待ちが発生する場合があるため、対策のためにkMaxDriftを引いた時間を下限にしている)
+	const auto sleepUntilLowerLimit = std::chrono::steady_clock::now() - kMaxDrift;
+	if (m_sleepUntil < sleepUntilLowerLimit)
 	{
-		m_sleepUntil = now;
+		m_sleepUntil = sleepUntilLowerLimit;
 	}
 
 	// sleepを実行
