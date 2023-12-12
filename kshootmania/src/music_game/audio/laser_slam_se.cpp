a3ae99f3a39240d62d167c790b17ecb4cfb136d4
masaka <masaka@mz-kb.com>
2023-12-13 06:02:02 +0900

    Add chokkakuautovol support

--- kshootmania/src/music_game/audio/laser_slam_se.cpp
+++ kshootmania/src/music_game/audio/laser_slam_se.cpp
@@ -36,22 +36,59 @@ namespace MusicGame::Audio
 			}
 
 			static_assert(kson::kNumLaserLanesSZ == 2U);
-			const auto& oppositeLaneStatus = gameStatus.laserLaneStatus[1U - i];
-			if (laneStatus.lastJudgedLaserSlamPulse == oppositeLaneStatus.lastJudgedLaserSlamPulse && m_lastPlayedTimeSecs[1U - i] >= oppositeLaneStatus.lastLaserSlamJudgedTimeSec)
-			{
-				// 2レーン同時直角の場合、他方のレーンで既に再生済みの場合は再生せず、既に再生済みの扱いにする
-				// (主にmaxPolyphonyが1でない旧譜面向けの二重再生対策を目的としている。他方のレーンでそれより後ろの直角を判定済みの場合には条件に漏れるが、実用上大きな問題はない)
-				m_lastPlayedTimeSecs[i] = laneStatus.lastLaserSlamJudgedTimeSec;
-				continue;
-			}
+			const std::size_t oppositeLaneIdx = 1U - i;
+			const kson::Pulse slamY = laneStatus.lastJudgedLaserSlamPulse;
 
 			// 直角音の音量を取得
-			// TODO: chokkakuautovol
 			const auto& volByPulse = chartData.audio.keySound.laser.vol;
-			const double volume = volByPulse.empty() ? kLaserSlamDefaultVolume : kson::ValueItrAt(volByPulse, laneStatus.lastJudgedLaserSlamPulse)->second;
+			const double volume = volByPulse.empty() ? kLaserSlamDefaultVolume : kson::ValueItrAt(volByPulse, slamY)->second;
+
+			const auto fnGetVolumeScaleByNote = [](const kson::ChartData& chartData, std::size_t laneIdx, kson::Pulse slamY) -> double
+			{
+				if (!chartData.audio.keySound.laser.legacy.volAuto)
+				{
+					return 1.0;
+				}
+
+				// 旧バージョンの直角音の音量を直角幅に応じて自動的に決めるオプション(chokkakuautovol)が有効の場合は、直角の横幅をもとに音量スケールを決定
+				const auto& lane = chartData.note.laser.at(laneIdx);
+				const auto graphSectionItr = kson::GraphSectionAt(lane, slamY);
+				if (graphSectionItr == lane.end())
+				{
+					assert(false && "graph section must exist here");
+					return 1.0;
+				}
+
+				const auto& [graphSectionY, graphSection] = *graphSectionItr;
+				const kson::RelPulse slamRy = slamY - graphSectionY;
+				if (!graphSection.v.contains(slamRy))
+				{
+					assert(false && "graph value must exist here");
+					return 1.0;
+				}
+				const auto& graphValue = graphSection.v.at(slamRy);
+				const double width = AbsDiff(graphValue.v, graphValue.vf) * graphSection.w;
+				return Clamp(width * 2, 0.0, 1.0);
+			};
+
+			const double volumeScaleByNote = fnGetVolumeScaleByNote(chartData, i, slamY);
+			const auto& oppositeLaneStatus = gameStatus.laserLaneStatus[oppositeLaneIdx];
+
+			// 2レーン同時直角の場合、大きい方の音量を優先する
+			if (slamY == oppositeLaneStatus.lastJudgedLaserSlamPulse)
+			{
+				const double oppositeVolumeScaleByNote = fnGetVolumeScaleByNote(chartData, oppositeLaneIdx, slamY);
+				if (oppositeVolumeScaleByNote > volumeScaleByNote ||
+					(oppositeVolumeScaleByNote == volumeScaleByNote && m_lastPlayedTimeSecs[oppositeLaneIdx] >= oppositeLaneStatus.lastLaserSlamJudgedTimeSec /* 最終再生時間が判定時間以降ということは再生済み */))
+				{
+					// 他方のレーンの方が音量が大きい場合は再生せず(もし完全に同じ音量の場合は他方のレーンで既に再生済みの場合は再生せず)、既に再生済みの扱いにする
+					m_lastPlayedTimeSecs[i] = laneStatus.lastLaserSlamJudgedTimeSec;
+					continue;
+				}
+			}
 
 			// 直角音を再生
-			m_sample.play(volume);
+			m_sample.play(volume * volumeScaleByNote);
 			m_lastPlayedTimeSecs[i] = laneStatus.lastLaserSlamJudgedTimeSec;
 		}
 	}
