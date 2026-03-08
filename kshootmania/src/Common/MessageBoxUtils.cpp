3f64152c72ab3bdf8830e69df4573f4fc6cf3652
masaka <masaka@mz-kb.com>
2026-03-08 20:35:38 +0900

    Yes/Noのメッセージボックス関数追加

--- kshootmania/src/Common/MessageBoxUtils.cpp
+++ kshootmania/src/Common/MessageBoxUtils.cpp
@@ -1,35 +1,47 @@
 ﻿#include "MessageBoxUtils.hpp"
 #ifdef _WIN32
 #include <Windows.h>
+
+namespace
+{
+	/// @brief MessageBoxStyleからWindowsのアイコンフラグに変換
+	UINT ToIconFlag(const MessageBoxStyle style)
+	{
+		switch (style)
+		{
+		case MessageBoxStyle::Info:
+			return MB_ICONINFORMATION;
+		case MessageBoxStyle::Warning:
+			return MB_ICONWARNING;
+		case MessageBoxStyle::Error:
+			return MB_ICONERROR;
+		case MessageBoxStyle::Question:
+			return MB_ICONQUESTION;
+		default:
+			return 0;
+		}
+	}
+}
 #endif
 
 MessageBoxResult MessageBoxUtils::ShowOK(const StringView text, const MessageBoxStyle style)
 {
 #ifdef _WIN32
-	UINT iconFlag = 0;
-	switch (style)
-	{
-	case MessageBoxStyle::Info:
-		iconFlag = MB_ICONINFORMATION;
-		break;
-	case MessageBoxStyle::Warning:
-		iconFlag = MB_ICONWARNING;
-		break;
-	case MessageBoxStyle::Error:
-		iconFlag = MB_ICONERROR;
-		break;
-	case MessageBoxStyle::Question:
-		iconFlag = MB_ICONQUESTION;
-		break;
-	default:
-		break;
-	}
-
 	const auto hWnd = static_cast<HWND>(Platform::Windows::Window::GetHWND());
-	::MessageBoxW(hWnd, text.toWstr().c_str(), Window::GetTitle().toWstr().c_str(), MB_OK | iconFlag);
-
+	::MessageBoxW(hWnd, text.toWstr().c_str(), Window::GetTitle().toWstr().c_str(), MB_OK | ToIconFlag(style));
 	return MessageBoxResult::OK;
 #else
 	return System::MessageBoxOK(text, style);
+#endif
+}
+
+MessageBoxResult MessageBoxUtils::ShowYesNo(const StringView text, const MessageBoxStyle style)
+{
+#ifdef _WIN32
+	const auto hWnd = static_cast<HWND>(Platform::Windows::Window::GetHWND());
+	const int result = ::MessageBoxW(hWnd, text.toWstr().c_str(), Window::GetTitle().toWstr().c_str(), MB_YESNO | ToIconFlag(style));
+	return result == IDYES ? MessageBoxResult::OK : MessageBoxResult::Cancel;
+#else
+	return System::MessageBoxOKCancel(text, style);
 #endif
 }
--- kshootmania/src/Common/MessageBoxUtils.hpp
+++ kshootmania/src/Common/MessageBoxUtils.hpp
@@ -4,5 +4,10 @@
 namespace MessageBoxUtils
 {
 	/// @brief フルスクリーン対応のMessageBoxOK(Windowsではフルスクリーン中でもOS標準ダイアログを表示)
+	/// @return 常にMessageBoxResult::OK
 	MessageBoxResult ShowOK(StringView text, MessageBoxStyle style = MessageBoxStyle::Default);
+
+	/// @brief フルスクリーン対応のMessageBoxYesNo(Windowsではフルスクリーン中でもOS標準ダイアログを表示)
+	/// @return 「はい」ならMessageBoxResult::OK、「いいえ」ならMessageBoxResult::Cancel
+	MessageBoxResult ShowYesNo(StringView text, MessageBoxStyle style = MessageBoxStyle::Default);
 }
