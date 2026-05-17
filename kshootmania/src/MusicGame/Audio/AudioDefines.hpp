5e66ef51b8cd4422bf69ced43029d73f4e5fb36a
masaka <masaka@mz-kb.com>
2026-05-17 17:22:50 +0900

    BASS更新に伴い効果音の先読みオフセットを35ms→70msに変更

--- kshootmania/src/MusicGame/Audio/AudioDefines.hpp
+++ kshootmania/src/MusicGame/Audio/AudioDefines.hpp
@@ -2,8 +2,8 @@
 
 namespace MusicGame::Audio
 {
-	// 効果音は35ms早めのタイミングで再生する
-	constexpr double kSELatencySec = 0.035;
+	// 効果音は70ms早めのタイミングで再生する
+	constexpr double kSELatencySec = 0.070;
 
 	// LASER直角音の最大同時再生数
 	// (旧バージョンの譜面では別の値を使う)
